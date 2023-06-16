e4ce72991fb149038a698ca8c335253c00436122
dweiller <4678790+dweiller@users.noreplay.github.com>
2023-06-16 15:54:08 +1000

    test: add basic usage tests

--- src/zimalloc.zig
+++ src/zimalloc.zig
@@ -6,3 +6,29 @@ test {
     _ = @import("Page.zig");
     _ = @import("Segment.zig");
 }
+
+test "basic validation" {
+    var gpa = Allocator.init(.{});
+    defer gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    try std.heap.testAllocator(allocator);
+    try std.heap.testAllocatorAligned(allocator);
+    try std.heap.testAllocatorLargeAlignment(allocator);
+    try std.heap.testAllocatorAlignedShrink(allocator);
+}
+
+test "create/destroy loop" {
+    var gpa = Allocator.init(.{});
+    defer gpa.deinit();
+    const allocator = gpa.allocator();
+
+    for (0..1000) |i| {
+        std.log.debug("iteration {d}", .{i});
+        var ptr = try allocator.create(u32);
+        allocator.destroy(ptr);
+    }
+}
+
+const std = @import("std");
