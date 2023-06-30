b579857ee1d054097dec01e16b45f22d455a495a
dweiller <4678790+dweiller@users.noreplay.github.com>
2023-06-30 16:16:12 +1000

    fix: align slices unmapped for huge alignment allocation

--- src/huge_alignment.zig
+++ src/huge_alignment.zig
@@ -1,3 +1,5 @@
+/// The `size` is  rounded up to a multiple of `std.mem.page_size`.
+/// Can be freed with std.os.unmap
 pub fn allocateOptions(
     size: usize,
     alignment: usize,
@@ -5,22 +7,30 @@ pub fn allocateOptions(
     flags: u32,
 ) ?[]align(std.mem.page_size) u8 {
     assert.withMessage(@src(), alignment > std.mem.page_size, "alignment is not greater than the page size");
+    assert.withMessage(@src(), std.mem.isValidAlign(alignment), "alignment is not a power of two");
+
     const mmap_length = size + alignment - 1;
     const unaligned = std.os.mmap(null, mmap_length, prot, flags, -1, 0) catch return null;
     const unaligned_address = @intFromPtr(unaligned.ptr);
     const aligned_address = std.mem.alignForward(usize, unaligned_address, alignment);
+
+    const aligned_size = std.mem.alignForward(usize, size, std.mem.page_size);
+
     if (aligned_address == unaligned_address) {
-        std.os.munmap(@alignCast(unaligned[size..]));
-        return unaligned;
+        std.os.munmap(@alignCast(unaligned[aligned_size..]));
+        return unaligned[0..aligned_size];
     } else {
         const offset = aligned_address - unaligned_address;
+        assert.withMessage(@src(), std.mem.isAligned(offset, std.mem.page_size), "offset is not aligned");
+
         std.os.munmap(unaligned[0..offset]);
-        std.os.munmap(@alignCast(unaligned[offset + size ..]));
-        return @alignCast(unaligned[offset..][0..size]);
+        std.os.munmap(@alignCast(unaligned[offset + aligned_size ..]));
+        return @alignCast(unaligned[offset..][0..aligned_size]);
     }
 }
 
-/// Makes a readable, writeable, anonymoust private mapping
+/// Makes a readable, writeable, anonymous private mapping with size rounded up to
+/// std.mem.page_size. Can be freed with std.os.unmap
 pub fn allocate(size: usize, alignment: usize) ?[]align(std.mem.page_size) u8 {
     return allocateOptions(
         size,
