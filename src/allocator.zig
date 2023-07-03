dweiller/zimalloc

commit 9bf43c15a5ab16cc5348763f0584441b2be686ca

fix: hold lock when calling Heap.deallocateHuge

src/allocator.zig
@@ -230,6 +230,8 @@ pub fn Allocator(comptime config: Config) type {
                 // TODO: this shouldn't be possible, heap.allocate() should respect
                 //       the limit, and this if statement can be replaced with an assert
                 if (self.stats.total_allocated_memory > limit) {
+                    heap.huge_allocations.lock();
+                    defer heap.huge_allocations.unlock();
                     _ = heap.deallocate(allocation.ptr[0..len], log2_align, ret_addr);
                     self.stats.total_allocated_memory -= allocation.backing_size;
                     return null;
@@ -323,7 +325,9 @@ pub fn Allocator(comptime config: Config) type {
                 return;
             };
 
+            heap.huge_allocations.lock();
             const size = heap.deallocateHuge(buf, log2_align, ret_addr);
+            heap.huge_allocations.unlock();
 
             if (config.memory_limit) |_| {
                 self.stats.total_allocated_memory -= size;

src/Heap.zig
@@ -236,8 +236,13 @@ pub fn deallocateHuge(self: *Heap, buf: []u8, log2_align: u8, ret_addr: usize) u
 // returns the backing size of the `buf`; behaviour is undefined if
 // `self` does not own `buf.ptr`.
 pub fn deallocate(self: *Heap, buf: []u8, log2_align: u8, ret_addr: usize) usize {
-    if (self.huge_allocations.contains(buf.ptr)) {
-        return self.deallocateHuge(buf, log2_align, ret_addr);
+    {
+        self.huge_allocations.lock();
+        defer self.huge_allocations.unlock();
+
+        if (self.huge_allocations.containsRaw(buf.ptr)) {
+            return self.deallocateHuge(buf, log2_align, ret_addr);
+        }
     }
     const segment = Segment.ofPtr(buf.ptr);
     return self.deallocateInSegment(segment, buf, log2_align, ret_addr);

commit 7216270b3b02f65bdb1cd8105c1e30d512fbdcfc

fix: remove deadlock introduced by 9bf43c1

src/allocator.zig
@@ -230,8 +230,6 @@ pub fn Allocator(comptime config: Config) type {
                 // TODO: this shouldn't be possible, heap.allocate() should respect
                 //       the limit, and this if statement can be replaced with an assert
                 if (self.stats.total_allocated_memory > limit) {
-                    heap.huge_allocations.lock();
-                    defer heap.huge_allocations.unlock();
                     _ = heap.deallocate(allocation.ptr[0..len], log2_align, ret_addr);
                     self.stats.total_allocated_memory -= allocation.backing_size;
                     return null;
@@ -325,9 +323,7 @@ pub fn Allocator(comptime config: Config) type {
                 return;
             };
 
-            heap.huge_allocations.lock();
             const size = heap.deallocateHuge(buf, log2_align, ret_addr);
-            heap.huge_allocations.unlock();
 
             if (config.memory_limit) |_| {
                 self.stats.total_allocated_memory -= size;